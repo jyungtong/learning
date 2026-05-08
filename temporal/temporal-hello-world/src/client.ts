ec6ccc1f26bcabb6e562c084c1aaaad05d0efd32
jyung <huntthetux@gmail.com>
2026-05-08 11:51:40 +0800

    temporal

---
+++ temporal/temporal-hello-world/src/client.ts
@@ -0,0 +1,27 @@
+import { Connection, Client } from '@temporalio/client';
+import { loadClientConnectConfig } from '@temporalio/envconfig';
+import { example } from './workflows';
+import { nanoid } from 'nanoid';
+
+async function run() {
+  const config = loadClientConnectConfig();
+  const connection = await Connection.connect(config.connectionOptions);
+  const client = new Client({ connection });
+
+  const handle = await client.workflow.start(example, {
+    taskQueue: 'hello-world',
+    // type inference works! args: [name: string]
+    args: ['WORLD!'],
+    // in practice, use a meaningful business ID, like customerId or transactionId
+    workflowId: 'workflow-' + nanoid(),
+  });
+  console.log(`Started workflow ${handle.workflowId}`);
+
+  // optional: wait for client result
+  console.log(await handle.result()); // Hello, Temporal!
+}
+
+run().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
